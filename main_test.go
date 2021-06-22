a648fcd88984b9e0a6f3ee868dca1adb15f574b3
Jef Spaleta <jspaleta@gmail.com>
2021-06-22 14:19:15 -0800

    add additional tests for sending metrics and event json log to sumo

--- main_test.go
+++ main_test.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
@@ -36,4 +42,87 @@ func TestConvertMetric(t *testing.T) {
 		assert.NotEqual(t, nsTime, dataString)
 		assert.NotEqual(t, sTime, dataString)
 	}
+}
+
+func TestSendMetrics(t *testing.T) {
+	event := corev2.FixtureEvent("entity1", "check1")
+	event.Check = nil
+	event.Metrics = corev2.FixtureMetrics()
+	msStamp := int64(1624376039373)
+	nsStamp := int64(1624376039373111122)
+	msTime := `answer{foo="bar"} 42 ` + fmt.Sprintf("%v", msStamp)
+	for _, p := range event.Metrics.Points {
+		p.Timestamp = nsStamp
+	}
+
+	var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err)
+		expectedBody := msTime
+		assert.Equal(t, expectedBody, strings.Trim(string(body), "\n"))
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	url, err := url.ParseRequestURI(test.URL)
+	assert.NoError(t, err)
+	plugin.Url = url.String()
+	dataString, err := convertMetrics(event)
+	assert.NoError(t, err)
+	assert.NoError(t, sendMetrics(dataString))
+}
+
+func TestSendLog(t *testing.T) {
+	event := corev2.FixtureEvent("entity1", "check1")
+	event.Check = nil
+	event.Metrics = nil
+	msgBytes, err := json.Marshal(event)
+	assert.NoError(t, err)
+
+	var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err)
+		expectedBody := string(msgBytes)
+		assert.Equal(t, expectedBody, strings.Trim(string(body), "\n"))
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	url, err := url.ParseRequestURI(test.URL)
+	assert.NoError(t, err)
+	plugin.Url = url.String()
+	assert.NoError(t, sendMetrics(string(msgBytes)))
+}
+
+func TestExecuteHandler(t *testing.T) {
+	event := corev2.FixtureEvent("entity1", "check1")
+	event.Check = nil
+	event.Metrics = corev2.FixtureMetrics()
+	msStamp := int64(1624376039373)
+	nsStamp := int64(1624376039373111122)
+	msTime := `answer{foo="bar"} 42 ` + fmt.Sprintf("%v", msStamp)
+	for _, p := range event.Metrics.Points {
+		p.Timestamp = nsStamp
+	}
+	msgBytes, err := json.Marshal(event)
+	plugin.Format = "prometheus"
+	plugin.AlwaysSendLog = true
+	var test = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err)
+		if len(r.Header["Content-Type"]) > 0 {
+			// recieved metrics with Content-Type header set
+			expectedBody := msTime
+			assert.Equal(t, expectedBody, strings.Trim(string(body), "\n"))
+		} else {
+			// recieved log with Content-Type header unset
+			expectedBody := string(msgBytes)
+			assert.Equal(t, expectedBody, strings.Trim(string(body), "\n"))
+		}
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	url, err := url.ParseRequestURI(test.URL)
+	assert.NoError(t, err)
+	plugin.Url = url.String()
+	err = executeHandler(event)
+	assert.NoError(t, err)
 }
